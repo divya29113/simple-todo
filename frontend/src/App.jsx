ec70a28c9d25fea88ae1ad756f98098688e94a19
divya29113 <divyajesus2003@gmail.com>
2026-03-26 23:43:39 +0530

    Add Ui design for todo app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+
+function App(){
+    const [todos,setTodos]=useState([]);
+    const[title,setTitle]=useState("");
+    const[loading,setLoading]=useState(false);
+
+    async function fetchTodos(){
+        try{
+            setLoading(true);
+            const todoResponse=await fetch("http://localhost:3000/api/todos");
+            const todoData=await todoResponse.json();
+            setTodos(todoData);
+        }
+        catch(err){
+            console.log("failed to fetch todos",err);
+
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
+    async function addTodo(e){
+        e.preventDefault();
+        if(!title.trim()) return;
+        try{
+            const addResponse=await fetch("http://locaalhost:3000/api/todos",{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({id:todos ? todos.lenght+1:1,title,completed:false})
+                })
+                const addData=await addResponse.json();
+                console.log(addData);
+                setTodos(addData.todos,...todos);
+
+                setTitle("");
+        }
+        catch(err){
+            console.log("failed to add data",err);
+        }
+    }
+
+    async function editTodo(id){
+        try{
+            let updateTodo=todos.map((todo)=>{
+                return todo.id===id?{...todo,completed:!todo.completed}:todo;
+
+            })
+
+            let toSendBackend=todos.find((todo)=>todo.id===id);
+            setTodos(updateTodo);
+            const editTodosResponse=await fetch(`http://localhost:3000/api/todos/${id}`{
+            method="PUT",
+            headers: {
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                ...toSendBackend
+            }),
+        });
+        console.log(editTodosResponse);
+    }catch(err){
+        console.log("failed to update",err);
+    }
+}
+
+async function deleteTodo(id){
+    try{
+        const deleteResponse=await fetch(`http://localhost:3000/api/todos/${id}`{
+            method:"DELETE",
+        });
+        const deleteData=todos.map((todo)=>todo.id!==id);
+        setTodos(deleteData);
+        console.log(deleteResponse);
+
+    }catch(err){
+        console.error("failed to delete data",err)
+    }
+}
+
+    return <div className="App">
+        <h1>TODO(MERN)</h1>
+        <form onSubmit={addTodo}>
+            <input type="text" placeholder="Add a new task ..." value={title} onChange={(e)=> setTitle(e.target.value)} />
+            <button type="submit">Add</button>
+            {loading && <p>loading todos...</p>}
+            <ul>
+                {todos.map((todo)=>(
+                    <li key={todo.id}>
+                        <span onClick={()=>editTodo(todo.id)}
+                        style={{textDecoration:todo.completed?"line-through":"none",
+                            cursor:"pointer",
+                        }}
+                        >
+                            {todo.title}
+                        </span>
+                        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+
+                    </li>
+                ))}
+            </ul>
+        </form>
+    </div>
+}
+
+export default App;
